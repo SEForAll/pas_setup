e1f9644a98d974b1fb7e77ae06ff1cf6c1e6471c
jack <myers395@purdue.edu>
2021-09-10 03:32:50 +0000

    modified add user script

--- servadmin/adduser.sh
+++ servadmin/adduser.sh
@@ -19,11 +19,12 @@ fi
 if [[ ! -d "/home/$1/.ssh" ]]
 then
 	mkdir /home/$1/.ssh
-	chmod 700 /home/$1/.ssh/
+	chmod 700 /home/$1/.ssh/ 
 fi
 
 if [[ -d "/mnt/usb/home/$1/" ]]
 then
+	echo "Adding Old Key"
 	cp /mnt/usb/home/$1/.ssh/authorized_keys /home/$1/.ssh/
 fi
 
@@ -35,6 +36,6 @@ then
 	read sshKey
 	echo "$sshKey" >> /home/$1/.ssh/authorized_keys
 fi
-	chmod 644 /home/$1/.ssh/authorized_keys
-
+chmod 644 /home/$1/.ssh/authorized_keys
+chown -R $1 /home/$1/.ssh/	
 exit 0
